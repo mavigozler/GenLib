cd56329cf2516fbab10f9fa8e1f37b4de79b0b07
TSMitch <stevepenner1960@gmail.com>
2023-08-14 15:36:12 -0700

    Updated with DWR SPTools modular

---
+++ createDTSFile.ts
@@ -0,0 +1,31 @@
+"use strict";
+
+import * as ts from "typescript";
+
+function compile(fileNames: string[], options: ts.CompilerOptions): void {
+  // Create a Program with an in-memory emit
+  const createdFiles: {[prop:string]: any} = {}
+  const host = ts.createCompilerHost(options);
+  host.writeFile = (fileName: string, contents: string) => createdFiles[fileName] = contents
+  
+  // Prepare and emit the d.ts files
+  const program = ts.createProgram(fileNames, options, host);
+  program.emit();
+
+  // Loop through all the input files
+  fileNames.forEach(file => {
+    console.log("### JavaScript\n")
+    console.log(host.readFile(file))
+
+    console.log("### Type Definition\n")
+    const dts = file.replace(".js", ".d.ts")
+    console.log(createdFiles[dts])
+  })
+}
+
+// Run the compiler
+compile(process.argv.slice(2), {
+  allowJs: true,
+  declaration: true,
+  emitDeclarationOnly: true,
+});
